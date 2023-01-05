57cb8526f187d6e18b46b3a5baef2a4fcb928891
Alex Santamaria <santamariaalex146@gmail.com>
2023-01-04 23:55:30 -0600

    fetching and displaying data in dom

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -10,7 +10,7 @@ const Books = () => {
     const fetchAllBooks = async () => {
       try {
         const res = await axios.get("http://localhost:8800/books");
-        console.log(res);
+        setBooks(res.data);
       } catch (error) {
         console.log(error);
       }
@@ -18,7 +18,21 @@ const Books = () => {
     fetchAllBooks();
   }, []);
 
-  return <div>Books</div>;
+  return (
+    <div>
+      <h1>Book Shop</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div className="book" key={book.id}>
+            {book.cover && <img src={book.cover} alt="" />}
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+            <span>{book.price}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Books;
