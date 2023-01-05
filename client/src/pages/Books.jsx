65daca7a70034e1ee8c8bed3cb7e382462a22f7e
Alex Santamaria <santamariaalex146@gmail.com>
2023-01-04 23:47:08 -0600

    i axios and cors to be able to fetch data from backend

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,6 +1,23 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/books");
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
   return <div>Books</div>;
 };
 
